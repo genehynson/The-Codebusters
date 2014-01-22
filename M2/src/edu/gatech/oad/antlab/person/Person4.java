b193770d607a37f2c19b17369aa99d3c59a1e779
Muhtadin <dean.m.hassan@gmail.com>
2014-01-22 12:55:21 -0500

    Edited Person4.java

--- M2/src/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/edu/gatech/oad/antlab/person/Person4.java
@@ -5,8 +5,8 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
- *  @version 1.1
+ *  @author Mohammad Muhtadin
+ *  @version 1.2
  */
 public class Person4 {
   /** Holds the persons real name */
