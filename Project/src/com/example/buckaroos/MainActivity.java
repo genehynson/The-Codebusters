2893cf62f6c8c1a94a1e6e829c0108021608309b
geneo933 <ghynson3@gatech.edu>
2014-01-30 14:05:37 -0500

    Created the basics for login screen.

--- Project/src/com/example/buckaroos/MainActivity.java
+++ Project/src/com/example/buckaroos/MainActivity.java
@@ -1,15 +1,37 @@
-package com.example.buckaroos;
+ package com.example.buckaroos;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener  {
+	
+	EditText etUser, etPass;
+	Button bLogin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		initialize();
+	}
+
+	private void initialize() {
+		etUser = (EditText) findViewById(R.id.etUser);
+		etPass = (EditText) findViewById(R.id.etPass);
+		bLogin = (Button) findViewById(R.id.bLogin);
+		
+		bLogin.setOnClickListener(this);
+	}
+	
+	@Override
+	public void onClick(View v) {
+		
 	}
 
 	@Override
