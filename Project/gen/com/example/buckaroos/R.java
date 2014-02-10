eeb52987c85ebc06a16b55971fd9bb71983c9b67
geneo933 <ghynson3@gatech.edu>
2014-02-10 14:20:26 -0500

    Attempting to add registering/logging in capability

--- Project/gen/com/example/buckaroos/R.java
+++ Project/gen/com/example/buckaroos/R.java
@@ -56,12 +56,16 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
-        public static final int activity_register=0x7f030001;
-        public static final int activity_welcome_screen=0x7f030002;
+        public static final int activity_login_success=0x7f030001;
+        public static final int activity_register=0x7f030002;
+        public static final int activity_register_success=0x7f030003;
+        public static final int activity_welcome_screen=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int register=0x7f080001;
+        public static final int login_success=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int register=0x7f080002;
+        public static final int regiter_success=0x7f080003;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
