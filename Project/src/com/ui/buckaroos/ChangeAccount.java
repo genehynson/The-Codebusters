26705d30eff7b7285d8dbfa3ab40371e3ef4efea
Daniel C <danielcarnauba8625@gmail.com>
2014-02-27 15:20:45 -0500

    Fixed the bug on ChangeAccount class

--- Project/src/com/ui/buckaroos/ChangeAccount.java
+++ Project/src/com/ui/buckaroos/ChangeAccount.java
@@ -1,3 +1,61 @@
+//package com.ui.buckaroos;
+//
+//import com.controller.buckaroos.UserAccountController;
+//import com.example.buckaroos.R;
+//import com.example.buckaroos.R.id;
+//import com.example.buckaroos.R.layout;
+//import com.example.buckaroos.R.menu;
+//
+//import android.os.Bundle;
+//import android.app.Activity;
+//import android.content.Intent;
+//import android.view.Menu;
+//import android.view.View;
+//import android.view.View.OnClickListener;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.Toast;
+//
+//public class ChangeAccount extends Activity implements OnClickListener {
+//
+//  private Button save;
+//  private EditText changeAccount;
+//  private UserAccountController controller;
+//  
+//  @Override
+//  protected void onCreate(Bundle savedInstanceState) {
+//      super.onCreate(savedInstanceState);
+//      setContentView(R.layout.activity_change_account);
+//      initialize();
+//  }
+//
+//  @Override
+//  public boolean onCreateOptionsMenu(Menu menu) {
+//      // Inflate the menu; this adds items to the action bar if it is present.
+//      getMenuInflater().inflate(R.menu.change_account, menu);
+//      return true;
+//  }
+//  
+//  public void initialize() {
+//      changeAccount = (EditText) findViewById(R.id.changeAccount);
+//      save = (Button) findViewById(R.id.saveChangeAccount);
+//      save.setOnClickListener(this);
+//      controller = new UserAccountController(this);
+//      getActionBar().hide();
+//  }
+//
+//  @Override
+//  public void onClick(View v) {
+//      if (controller.getUserAccount(changeAccount.getText().toString()) != null) {
+//          controller.setCurrentAccount(controller.getUserAccount(changeAccount.getText().toString()));
+//          startActivity(new Intent(ChangeAccount.this, LoginSuccess.class));
+//      } else {
+//          Toast toast = Toast.makeText(this, "No such account.", Toast.LENGTH_SHORT);
+//          toast.show();
+//      }
+//  }
+//
+//}
 package com.ui.buckaroos;
 
 import java.text.NumberFormat;
@@ -9,11 +67,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.controller.buckaroos.UserAccountController;
 import com.example.buckaroos.R;
@@ -25,7 +81,6 @@ import com.model.buckaroos.Account;
  */
 public class ChangeAccount extends Activity {
     private List<Account> userAccounts = new ArrayList<Account>();
-//    private ListView listView;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,23 +91,21 @@ public class ChangeAccount extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-    	setContentView(R.layout.activity_account_overview);
-    	populateAccountList();
-    	populateListView();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_account);
+        populateAccountList();
+        populateListView();
 
-//    	listView.setOnItemClickListener(new OnItemClickListener() {
-//    		public void onItemClick(AdapterView<?> parent, View view,
-//    				int position, long id) {
-//    			// When clicked, show a toast with the TextView text
-//    			Toast.makeText(getApplicationContext(),
-//    					((TextView) view).getText(), Toast.LENGTH_SHORT).show();
-//    		}
-//    	});
-//
+        // listView.setOnItemClickListener(new OnItemClickListener() {
+        // public void onItemClick(AdapterView<?> parent, View view,
+        // int position, long id) {
+        // // When clicked, show a toast with the TextView text
+        // Toast.makeText(getApplicationContext(),
+        // ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+        // }
+        // });
+
     }
-    
-    
 
     private void populateAccountList() {
         UserAccountController controller = new UserAccountController(this);
@@ -94,8 +147,3 @@ public class ChangeAccount extends Activity {
         }
     }
 }
-
-//controller.setCurrentAccount(controller.getUserAccount(changeAccount.getText().toString()));
-//startActivity(new Intent(ChangeAccount.this, LoginSuccess.class));
-
-
