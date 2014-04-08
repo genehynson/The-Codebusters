b91918f333bc68b1cd987f4842dd6fa78e4da860
Jordan L <jsl20038@gmail.com>
2014-04-07 20:35:33 -0400

    Updated PasswordHash class to use MD5

--- BuckaroosGWT/src/com/buckaroos/utility/PasswordHash.java
+++ BuckaroosGWT/src/com/buckaroos/utility/PasswordHash.java
@@ -1,7 +1,7 @@
 package com.buckaroos.utility;
 
-import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Class designed to provide a hashed version of the password for security
@@ -11,37 +11,20 @@ import java.security.MessageDigest;
  */
 public class PasswordHash {
 
-    public static int PW_HASH_ITERATION_COUNT = 5000;
-    private static MessageDigest md;
+    private MessageDigest md;
 
     public String hashPassword(String pw) {
-        String salt = "rjelk;a903u29ujmfadkls;09432ujop;zad";
-        byte[] bSalt;
-        byte[] bPw;
-
-        try {
-            bSalt = salt.getBytes("UTF-8");
-            bPw = pw.getBytes("UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("Unsupported Encoding", e);
-        }
-
-        byte[] digest = run(bPw, bSalt);
-        for (int i = 0; i < PW_HASH_ITERATION_COUNT - 1; i++) {
-            digest = run(digest, bSalt);
-        }
-
         StringBuffer sb = new StringBuffer();
-        for (byte b : digest) {
-            sb.append(Integer.toHexString((int) (b & 0xff)));
+        try {
+            md = MessageDigest.getInstance("MD5");
+            md.update(pw.getBytes());
+            byte[] digest = md.digest();
+            for (byte b : digest) {
+                sb.append(Integer.toHexString((int) (b & 0xff)));
+            }
+        } catch (NoSuchAlgorithmException e1) {
+            // Do Nothing
         }
-
         return sb.toString();
     }
-
-    private static byte[] run(byte[] input, byte[] salt) {
-        md.update(input);
-        return md.digest(salt);
-    }
-
 }
