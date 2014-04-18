bb21780e287e8a5ddefe1e56624608df0e84b6c0
Jordan L <jsl20038@gmail.com>
2014-04-18 08:48:41 -0400

    Created CurrencyConverter that supports live exchange rates.

--- Buckaroos Model/src/com/utility/buckaroos/CurrencyConverter.java
+++ Buckaroos Model/src/com/utility/buckaroos/CurrencyConverter.java
@@ -1,32 +1,60 @@
 package com.utility.buckaroos;
 
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 /**
  * This class supports conversion from one type of currency to another.
  * Supported currencies are USD, AUD, BRL, CAD, CNY, EUR, GBP, JPY, INR, CHF,
  * RUB, MXN, AED, and BDT.
  * 
  * @author Jordan LeRoux
- * @version 1.0
+ * @version 1.1
  */
+@SuppressWarnings("deprecation")
 public class CurrencyConverter implements CurrencyConverterInterface {
 
-    private static final double FROMUSDTOAUD = 1.1206;
-    private static final double FROMUSDTOBRL = 2.3443;
-    private static final double FROMUSDTOCAD = 1.1064;
-    private static final double FROMUSDTOCNY = 6.1451;
-    private static final double FROMUSDTOEUR = 0.7245;
-    private static final double FROMUSDTOGBP = 0.5972;
-    private static final double FROMUSDTOJPY = 101.8;
-    private static final double FROMUSDTOINR = 61.7575;
-    private static final double FROMUSDTOCHF = 0.8803;
-    private static final double FROMUSDTORUB = 35.8641;
-    private static final double FROMUSDTOMXN = 13.2492;
-    private static final double FROMUSDTOAED = 3.673;
-    private static final double FROMUSDTOBDT = 77.715;
+    private static double FROMUSDTOAUD = 1.1206;
+    private static double FROMUSDTOBRL = 2.3443;
+    private static double FROMUSDTOCAD = 1.1064;
+    private static double FROMUSDTOCNY = 6.1451;
+    private static double FROMUSDTOEUR = 0.7245;
+    private static double FROMUSDTOGBP = 0.5972;
+    private static double FROMUSDTOJPY = 101.8;
+    private static double FROMUSDTOINR = 61.7575;
+    private static double FROMUSDTOCHF = 0.8803;
+    private static double FROMUSDTORUB = 35.8641;
+    private static double FROMUSDTOMXN = 13.2492;
+    private static double FROMUSDTOAED = 3.673;
+    private static double FROMUSDTOBDT = 77.715;
+
+    /**
+     * Fetch the current exchange rates whenever constructed
+     */
+    public CurrencyConverter() {
+        // Consider adding a table to the database and then storing a date in
+        // the database and compare the date in the database with the current
+        // date and if they don't match to the hour, then set the exchange
+        // rates.
+        // This would prevent us having to update the exchange rates every time
+        // a conversion is called because this can be quite slow when first
+        // constructed.
+        setExchangeRates();
+    }
 
     @Override
     public double convertCurrency(Enum<Money> fromCurrency,
             Enum<Money> toCurrency, double amount) {
+        if (fromCurrency == null || toCurrency == null) {
+            throw new IllegalArgumentException("Can't convert from null or to "
+                    + "null currency.");
+        }
         double newAmount = 0;
         if (fromCurrency != Money.valueOf("USD")
                 && toCurrency != Money.valueOf("USD")) {
@@ -44,6 +72,62 @@ public class CurrencyConverter implements CurrencyConverterInterface {
             newAmount = amount;
         }
         return newAmount;
+    }
+
+    /*
+     * Gets the current exchange rate for the currencies passed in.
+     * 
+     * @param fromCurrency The currency to convert from.
+     * 
+     * @param toCurrency The currency to convert to.
+     * 
+     * @return The exchange rate for the currencies.
+     * 
+     * @throws ClientProtocolException
+     * 
+     * @throws IOException
+     */
+    private static float getExchangeRates(Enum<Money> fromCurrency,
+            Enum<Money> toCurrency) throws ClientProtocolException, IOException {
+        @SuppressWarnings("resource")
+        HttpClient client = new DefaultHttpClient();
+
+        HttpGet a = new HttpGet("http://quote.yahoo.com/d/quotes.csv?s="
+                + fromCurrency + toCurrency + "=X&f=l1&e=.csv");
+        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+        String responseBody = client.execute(a, responseHandler);
+
+        client.getConnectionManager().shutdown();
+        return Float.parseFloat(responseBody);
+    }
+
+    /*
+     * Sets the current exchange rates for every currency that Buckaroos
+     * supports.
+     */
+    private static void setExchangeRates() {
+        try {
+            FROMUSDTOAUD = getExchangeRates(Money.USD, Money.AUD);
+            FROMUSDTOBRL = getExchangeRates(Money.USD, Money.BRL);
+            FROMUSDTOCAD = getExchangeRates(Money.USD, Money.CAD);
+            FROMUSDTOCNY = getExchangeRates(Money.USD, Money.CNY);
+            FROMUSDTOEUR = getExchangeRates(Money.USD, Money.EUR);
+            FROMUSDTOGBP = getExchangeRates(Money.USD, Money.GBP);
+            FROMUSDTOJPY = getExchangeRates(Money.USD, Money.JPY);
+            FROMUSDTOINR = getExchangeRates(Money.USD, Money.INR);
+            FROMUSDTOCHF = getExchangeRates(Money.USD, Money.CHF);
+            FROMUSDTORUB = getExchangeRates(Money.USD, Money.RUB);
+            FROMUSDTOMXN = getExchangeRates(Money.USD, Money.MXN);
+            FROMUSDTOAED = getExchangeRates(Money.USD, Money.AED);
+            FROMUSDTOBDT = getExchangeRates(Money.USD, Money.BDT);
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     /*
